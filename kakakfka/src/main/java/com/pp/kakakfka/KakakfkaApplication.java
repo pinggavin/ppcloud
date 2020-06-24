384e2962976f4e25c41c059ec16aebb1b37bc24d
gavinzhang <228632062@qq.com>
2020-06-24 15:37:59 +0800

    kafka

---
+++ kakakfka/src/main/java/com/pp/kakakfka/KakakfkaApplication.java
@@ -0,0 +1,13 @@
+package com.pp.kakakfka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakakfkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakakfkaApplication.class, args);
+    }
+
+}
